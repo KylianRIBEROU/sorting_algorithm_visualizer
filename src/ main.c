88cdfbc10bd86df672d067ca8583d55fd33ce71b
ChabilanLeni <chabilan.leni@gmail.com>
2025-10-27 13:08:07 +0100

    Fichier de test

--- src/ main.c
+++ src/ main.c
@@ -2,3 +2,42 @@
 // Created by kylia on 27/10/2025.
 //
 // point d'entrée et affichage SDL
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Test SDL2",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          800, 600, 0);
+    if (!window) {
+        printf("Error creating window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255); // bleu clair
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    // Boucle d'événements
+    SDL_Event e;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) quit = 1;
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
